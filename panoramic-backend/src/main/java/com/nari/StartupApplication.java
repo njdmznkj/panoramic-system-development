994030e0e0d4f501ce397e78557e688cf2b1da20
zhangheng <372237882@qq.com>
2022-04-20 16:50:35 +0800

    init project

--- panoramic-backend/src/main/java/com/nari/StartupApplication.java
+++ panoramic-backend/src/main/java/com/nari/StartupApplication.java
@@ -1,15 +1,37 @@
 package com.nari;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+import org.springframework.util.StringUtils;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
- *  启动类
+ * 启动类
  */
 @SpringBootApplication
+@Slf4j
 public class StartupApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(StartupApplication.class,args);
+    public static void main(String[] args) throws UnknownHostException {
+
+        ConfigurableApplicationContext application = SpringApplication.run(StartupApplication.class, args);
+
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        if (StringUtils.isEmpty(path)) {
+            path = "";
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application  is running! Access URLs:\n\t" +
+                "Local访问网址: \t\thttp://localhost:" + port + path + "\n\t" +
+                "External访问网址: \thttp://" + ip + ":" + port + path + "\n\t" +
+                "----------------------------------------------------------");
     }
 }
